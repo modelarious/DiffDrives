1d53e0c0ceb0f4ec44e93787de146fa89b8fa80d
modelarious <michaelwilliamhackman@gmail.com>
2019-09-20 13:51:57 -0600

    imported DiffDrives main function and hooked it into testing

--- DirectoryAndFileTests.py
+++ DirectoryAndFileTests.py
@@ -1,7 +1,10 @@
-# content of test_expectation.py
 import pytest
+from DiffDrives import main
 
 BaseDir = "Testing/"
+TargetA = "DiffTargetA"
+TargetB = "DiffTargetB"
+
 Directories = ['SameDirectoryStructureFlat', 'SameDirectoryStructureNested', 'DifferentDirectoryStructureFlat', 'DifferentDirectoryStructureNested', 'SameFilesFlat', 'SameFilesNested', 'DifferentFilesFlat', 'DifferentFilesNested', 'DifferentDirectoryStructureDifferentFilesFlat', 'DifferentDirectoryStructureDifferentFilesNested']
 
 ExpectedOutputs = ["Hello World"] * len(Directories)
@@ -11,4 +14,5 @@ TestCaseNums = ["JUJU"] * len(Directories)
 @pytest.mark.parametrize("directory, expected_output, testcase_num", list(zip(Directories, ExpectedOutputs, TestCaseNums)))
 def test_eval(directory, expected_output, testcase_num):
 	print(BaseDir + directory, expected_output, testcase_num)
+	main(BaseDir + directory + "/" + TargetA, BaseDir + directory + "/" + TargetB)
 	#assert directory == expected_output

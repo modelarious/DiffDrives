59e531bd3961deff8b2f0cbb8bce90b164186d1e
modelarious <michaelwilliamhackman@gmail.com>
2019-10-04 15:28:55 -0600

    started calling os.walk on each directory shared by both targets

--- DiffDrives.py
+++ DiffDrives.py
@@ -4,61 +4,6 @@ from sys import argv
 import subprocess
 import os
 
-
-'''
-[('Testing/DifferentDirectoryStructureDifferentFilesFlat', ['DiffTargetA', 'DiffTargetB'], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA', ['A', 'B', 'C'], ['fileA.txt', 'fileB.txt', 'fileC.txt']), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA/A', [], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA/B', [], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA/C', [], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetB', ['A', 'B'], ['fileA.txt', 'fileB.txt']), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetB/A', [], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetB/B', [], [])]
-'''
-class DiffDrives:
-
-	#various files that don't need to be considered
-	skippedFiles = [".DS_Store"]
-
-	def __init__(self, verifiedPathA, verifiedPathB):
-		self.pathA = verifiedPathA
-		self.pathB = verifiedPathB
-		print(f"INIT with {self.pathA} and {self.pathB}")
-		print(self._captureChild(self.pathA))
-
-	def _captureChild(self, path):
-		return self._captureDirectories(path), self._captureFiles(path)
-
-	'''
-	returns a list of child directories for the given path
-	'''
-	def _captureDirectories(self, path):
-		#[('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA', ['A', 'B', 'C'], ['fileA.txt', 'fileB.txt', 'fileC.txt']), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA/A', [], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA/B', [], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA/C', [], [])]
-		dirWalk = list(os.walk(path))
-		childDirectories = dirWalk[0][1]
-		return childDirectories
-	'''
-	returns a list of files in child directory for given path
-	'''
-	def _captureFiles(self, path):
-		#[('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA', ['A', 'B', 'C'], ['fileA.txt', 'fileB.txt', 'fileC.txt']), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA/A', [], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA/B', [], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA/C', [], [])]
-		dirWalk = list(os.walk(path))
-		childFiles = [f for f in dirWalk[0][2] if f not in self.skippedFiles]
-		return childFiles
-
-	'''
-	run methods that will process two directories
-	'''
-	def processDiff(self):
-		return self._captureChild(self.pathA), self._captureChild(self.pathB)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #various files that don't need to be considered
 skippedFiles = [".DS_Store"]
 
@@ -89,61 +34,65 @@ def getNext(generator):
 '''
 [('Testing/DifferentDirectoryStructureDifferentFilesFlat', ['DiffTargetA', 'DiffTargetB'], [".DS_Store"]), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA', ['A', 'B', 'C'], ['fileA.txt', 'fileB.txt', 'fileC.txt']), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA/A', [], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA/B', [], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA/C', [], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetB', ['A', 'B'], ['fileA.txt', 'fileB.txt']), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetB/A', [], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetB/B', [], [])]
 '''
+def compare(children):
+	print("calling compare with", children)
+	#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [], [])
+	#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [], [])
+	#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], [])
+	#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], [])
+	#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A', 'B'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], [])
+	#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A', 'B'], [])
+	diffDict = dict()
+
+	while True:
+		nextA = getNext(children["A"])
+		nextB = getNext(children["B"])
+
+		#We're only looking for what's in A that isn't in B, so we don't care if children of B is None, we only worry about when we've exhausted children of A
+		if nextA == None:
+			print("Exhausted A, returning diff dict")
+			return diffDict
+
+		pathA, dirsA, filesA = getDetails(nextA)
+		pathB, dirsB, filesB = getDetails(nextB)
+		
+		print(pathA, dirsA, filesA, "||", pathB, dirsB, filesB)
+		print("dirs in B and in A:", [x for x in dirsB if x in dirsA])
+		print("dirs in A but not in B:", [x for x in dirsA if x not in dirsB])
+
+		union = [x for x in dirsB if x in dirsA]
+		inAbutNotB = [x for x in dirsA if x not in dirsB]
+		
+		origPathA = "Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA"
+		origPathB = "Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB"
+		
+		for childofBoth in union:
+			children = dict({"A":getChildrenGenerator(origPathA + "/" + childofBoth), "B":getChildrenGenerator(origPathB + "/" + childofBoth)})
+			print(compare(children))
+		break
+		
+
 def main(pathA, pathB):
 	print(f"main called with {pathA}, {pathB}")
 	#verify paths exist before walking them
 	if path.exists(pathA) and path.exists(pathB):
 		children = dict({"A":getChildrenGenerator(pathA), "B":getChildrenGenerator(pathB)})
-		
-		#root directory, needs a special case
-		#FirstA = getNext(children["A"])
-		#FirstB = getNext(children["B"])
-		
-		#if children of A don't exist, return
-		#We're only looking for what's in A that isn't in B, so we don't care if children of B is None, we only worry about when we've exhausted children of A
-		#if FirstA == None:
-		#	return
-
-		#Case: both directory lists empty
-		#compare files
-		#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [], [])
-
-		#Case: TargetA has a directory, TargetB has none
-		#Include TargetA's directory in the results
-		#compare files
-		#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [], [])    
-		#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], [])    
-		#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], [])
-		#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A', 'B'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], [])
-		#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A', 'B'], [])
-		while True:
-			nextA = getNext(children["A"])
-			nextB = getNext(children["B"])
-			
-			#We're only looking for what's in A that isn't in B, so we don't care if children of B is None, we only worry about when we've exhausted children of A
-			if nextA == None:
-				return
-
-			pathA, dirsA, filesA = getDetails(nextA)
-			nextA = getDetails(nextA)
-			nextB = getDetails(nextB)
-
-			print(nextA, nextB)
-
-		'''
+		#return compare(childA, childB, pathA, pathB)
+		return compare(children)
+	'''
 		pointer = 0
 		maxSizeOfChilren = max(map(len, children.values()))
 		while pointer < maxSizeOfChilren:
 			dirs = dict({k:getDirs(v[0]) for k,v in children.items()})	
 			print(dirs)
 			pointer += 1
-		'''
+	'''
 		#('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA', ['A', 'B', 'C'], ['fileA.txt', 'fileB.txt', 'fileC.txt']), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA/A', [], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA/B', [], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetA/C', [], [])
 		#('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetB', ['A', 'B'], ['fileA.txt', 'fileB.txt']), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetB/A', [], []), ('Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetB/B', [], [])
 		
 		
-	else:
-		return -1
+	#else:
+	#	return -1
 		
 
 if __name__ == "__main__":

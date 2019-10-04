243f842dec2fe08d05f4cc75e397e8bbc0a6fec3
modelarious <michaelwilliamhackman@gmail.com>
2019-10-04 15:05:37 -0600

    Developped the generator solution, enumerated equivalence classes

--- DiffDrives.py
+++ DiffDrives.py
@@ -62,11 +62,21 @@ class DiffDrives:
 #various files that don't need to be considered
 skippedFiles = [".DS_Store"]
 
+def getDetails(nameDirsFilesTuple):
+	tup = nameDirsFilesTuple
+	if tup == None:
+		return None, None, None
+	return getPath(tup), getDirs(tup), getFiles(tup)
+
+def getPath(nameDirsFilesTuple):
+	return nameDirsFilesTuple[0]
+
 def getDirs(nameDirsFilesTuple):
 	return nameDirsFilesTuple[1]
 
 def getFiles(nameDirsFilesTuple):
 	return [f for f in nameDirsFilesTuple[2] if f not in skippedFiles]
+
 
 def getChildrenGenerator(path):
 	return os.walk(path)
@@ -85,18 +95,39 @@ def main(pathA, pathB):
 	if path.exists(pathA) and path.exists(pathB):
 		children = dict({"A":getChildrenGenerator(pathA), "B":getChildrenGenerator(pathB)})
 		
-		FirstA = getNext(children["A"])
-		FirstB = getNext(children["B"])
+		#root directory, needs a special case
+		#FirstA = getNext(children["A"])
+		#FirstB = getNext(children["B"])
+		
+		#if children of A don't exist, return
+		#We're only looking for what's in A that isn't in B, so we don't care if children of B is None, we only worry about when we've exhausted children of A
+		#if FirstA == None:
+		#	return
 
-		#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], ['.DS_Store']) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], ['.DS_Store'])
+		#Case: both directory lists empty
+		#compare files
+		#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [], [])
+
+		#Case: TargetA has a directory, TargetB has none
+		#Include TargetA's directory in the results
+		#compare files
+		#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [], [])    
+		#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], [])    
+		#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], [])
+		#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A', 'B'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], [])
+		#('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A', 'B'], [])
 		while True:
 			nextA = getNext(children["A"])
 			nextB = getNext(children["B"])
-
+			
 			#We're only looking for what's in A that isn't in B, so we don't care if children of B is None, we only worry about when we've exhausted children of A
 			if nextA == None:
 				return
-			
+
+			pathA, dirsA, filesA = getDetails(nextA)
+			nextA = getDetails(nextA)
+			nextB = getDetails(nextB)
+
 			print(nextA, nextB)
 
 		'''
--- DirectoryAndFileTests.py
+++ DirectoryAndFileTests.py
@@ -1,6 +1,30 @@
 import pytest
 from DiffDrives import main
+'''
+needed
+each of the following cases for the top level directory:
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [], [])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [], [])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], [])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], [])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A', 'B'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], [])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A', 'B'], [])
 
+with each of the following variations based on files
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [*], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [*], [])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [*], ['A']) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [*], [])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [*], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [*], ['A'])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [*], ['A']) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [*], ['A'])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [*], ['A', 'B']) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [*], ['A'])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [*], ['A']) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [*], ['A', 'B'])
+
+
+Test existing tests (like SameDirectoryStructureFlat) with each of the operands TargetA and TargetB swapped with a new set of expected outputs
+
+test that an empty A will report that correctly and quickly
+
+test that a large example will not mess up
+'''
 BaseDir = "Testing/"
 TargetA = "DiffTargetA"
 TargetB = "DiffTargetB"

690be3f15f9974e57056cb80c556b6ad4dcd5cc1
modelarious <michaelwilliamhackman@gmail.com>
2019-11-20 22:19:07 -0700

    Got tests passing apart from the very_hard test

---
+++ ParallelTests.py
@@ -0,0 +1,75 @@
+import pytest
+import ParralelDiffDrives
+import DiffDrives
+'''
+needed
+each of the following cases for the top level directory:
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [], [])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [], [])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], [])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], [])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A', 'B'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A'], [])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', ['A'], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', ['A', 'B'], [])
+
+with each of the following variations based on files
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [*], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [*], [])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [*], ['A']) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [*], [])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [*], []) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [*], ['A'])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [*], ['A']) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [*], ['A'])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [*], ['A', 'B']) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [*], ['A'])
+                #('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA', [*], ['A']) ('Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB', [*], ['A', 'B'])
+
+
+Test existing tests (like SameDirectoryStructureFlat) with each of the operands TargetA and TargetB swapped with a new set of expected outputs
+
+test that an empty A will report that correctly and quickly
+
+test that a large example will not mess up
+'''
+BaseDir = "Testing/"
+TargetA = "DiffTargetA"
+TargetB = "DiffTargetB"
+
+Directories = ['SameDirectoryStructureFlat', 'SameDirectoryStructureNested', 'DifferentDirectoryStructureFlat', 'DifferentDirectoryStructureNested', 'SameFilesFlat', 'SameFilesNested', 'DifferentFilesFlat', 'DifferentFilesNested', 'DifferentDirectoryStructureDifferentFilesFlat', 'DifferentDirectoryStructureDifferentFilesNested']
+
+ExpectedOutputs = [
+	{'dirs': [], 'files': []}, 
+	{'dirs': [], 'files': []},
+	{'dirs': ['Testing/DifferentDirectoryStructureFlat/DiffTargetB/C'], 'files': []},
+	{'dirs': ['Testing/DifferentDirectoryStructureNested/DiffTargetB/A/F'], 'files': []},
+	{'dirs': [], 'files': []},
+	{'dirs': [], 'files': []},
+	{'dirs': [], 'files': ['Testing/DifferentFilesFlat/DiffTargetB/fileC.txt']},
+	{'dirs': [], 'files': ['Testing/DifferentFilesNested/DiffTargetB/A/E/fileC.txt']},
+	{'dirs': ['Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetB/C'], 'files': ['Testing/DifferentDirectoryStructureDifferentFilesFlat/DiffTargetB/fileC.txt']},
+	{'dirs': ['Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB/A/F'], 'files': ['Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB/A/E/fileC.txt']}]
+
+	
+TestCaseNums = [f"TC_{i+1:03d}" for i in range(len(Directories))]
+
+
+@pytest.mark.parametrize("directory, expected_output, testcase_num", list(zip(Directories, ExpectedOutputs, TestCaseNums)))
+def test_eval(directory, expected_output, testcase_num):
+	print(BaseDir + directory, expected_output, testcase_num)
+	
+	serialResult = DiffDrives.main(BaseDir + directory + "/" + TargetA, BaseDir + directory + "/" + TargetB)
+	parralelResult = ParralelDiffDrives.main(BaseDir + directory + "/" + TargetA, BaseDir + directory + "/" + TargetB)
+	assert ( serialResult == expected_output)
+	assert ( parralelResult == serialResult)
+	
+'''
+def test_hard():
+	path1 = "/Volumes/MyRAID/Games"
+	path2 = "/Volumes/FUCKYOUWIND/Games"
+	serialResult = DiffDrives.main(path1, path2)
+	parallelResult = ParralelDiffDrives.main(path1, path2)
+	assert( parallelResult == serialResult)
+
+
+def test_very_hard():
+	path1 = "/Volumes/MyRAID"
+	path2 = "/Volumes/FUCKYOUWIND"
+	serialResult = DiffDrives.main(path1, path2)
+	parallelResult = ParralelDiffDrives.main(path1, path2)
+	self.assertDictEqual( parallelResult, serialResult)
+'''
--- ParralelDiffDrives.py
+++ ParralelDiffDrives.py
@@ -3,18 +3,37 @@ import threading
 import time
 import logging
 import queue
+import pprint
 
-LOGGING = True
+'''
+DEBUG, STATUS and LOGGING are for debug purposes and just change the level of logging that is occuring.
+These don't ever need to be used unless you want more detail into what is going on
+'''
+DEBUG = False
+def printif(*args):
+	if DEBUG == True:
+		print(args)
+
+STATUS = False
+def printSTATUS(*args):
+	if STATUS == True:
+		print(args)
+		
+LOGGING = False
 if LOGGING:
 	logging.basicConfig(level=logging.DEBUG,
                     format='(%(threadName)-9s) %(message)s',)
 
+hasBeenInit = False #if this file has been initialized already, then don't make more threads
 queueSizeLock = threading.Lock()
 queueSize = 1
 ProcessingFinished = threading.Condition()
 q = queue.Queue()
+diffSolu = {"files":[], "dirs":[]}
 
 class ConsumerThread(threading.Thread):
+
+	skippedFiles = [".DS_Store"]
 
 	def _getDirInfo(self, receivedPath):
 		path, dirs, files = "",[],[]
@@ -23,6 +42,7 @@ class ConsumerThread(threading.Thread):
 		#those directories raise a StopIteration error.
 		try:
 			path, dirs, files = next(walk(receivedPath))
+			files = [f for f in files if f not in self.skippedFiles]
 		except StopIteration:
 			pass
 		return path, dirs, files
@@ -32,39 +52,64 @@ class ConsumerThread(threading.Thread):
 		while True:
 			if not q.empty():
 				receivedPathA, receivedPathB  = q.get()
-				print(receivedPathA, receivedPathB)
+				printif(receivedPathA, receivedPathB)
 				
 				pathA, dirsA, filesA = self._getDirInfo(receivedPathA)
 				pathB, dirsB, filesB = self._getDirInfo(receivedPathB)
-				
-				print(self._getDirInfo(receivedPathA))
-				print(self._getDirInfo(receivedPathB))
+				'''
+				printif(self._getDirInfo(receivedPathA))
+				printif(self._getDirInfo(receivedPathB))
 				
 				logging.debug('Got ' + str(pathA) + str(pathB))
-				
-				dirs = dirsA
-				path = pathA
-				
+				'''
 				#heavy processing here
+				union = [x for x in dirsB if x in dirsA]
+				inAbutNotB = [x for x in dirsA if x not in dirsB]
+				filesInAbutNotInB = [x for x in filesA if x not in filesB]
+				'''
+				printif(pathA, dirsA, filesA, "||", pathB, dirsB, filesB)
+				printSTATUS("dirs in B and in A:", union)
+				printSTATUS("dirs in A but not in B:", inAbutNotB)
+				'''
+				#add the path to everything in "inAbutNotB" and track it
+				dirExtension = [pathB + sep + x for x in inAbutNotB] #XXX this should be pathA and so should the one below
+				fileExtension = [pathB + sep + x for x in filesInAbutNotInB] 
+				'''
+				printSTATUS("extending the dir solution by", dirExtension)
+				printSTATUS("extending the file solution by", fileExtension)
+				'''
+				diffSolu["dirs"].extend(dirExtension)
+				diffSolu["files"].extend(fileExtension)
+				
+				'''
+				if dirExtension != []:
+					print("extending the dir solution by", dirExtension, "as I received", pathA, dirsA, filesA, "||", pathB, dirsB, filesB)
+				if fileExtension != []:
+					print("extending the file solution by", fileExtension, "as I received", pathA, dirsA, filesA, "||", pathB, dirsB, filesB)
+				'''
 
 				with queueSizeLock:
-					for dir in dirs:
+					#printif("Putting in", union)
+					for childOfBoth in union:
                     	#construct path: "path/to/current/directory" + "/" + "sub_directory"
-						item = (path + sep + dir, path + sep + dir) 
+						item = (pathA + sep + childOfBoth, pathB + sep + childOfBoth) 
 						q.put(item)
-						logging.debug('Putting ' + str(item))
-                
+						#logging.debug('Putting ' + str(item))
+                	
 					'''
             		We have finished with this queue entry, so we subtract 1 
-            		from the queueSize.  We have also found len(dirs) new entries 
+            		from the queueSize.  We have also found len(union) new entries 
             		for the queue, so we add that to queueSize
             		'''
-					queueSize += len(dirs) - 1
+					logging.debug('initial queue size ' + str(queueSize))
+					queueSize += len(union) - 1
+					logging.debug('after queue size ' + str(queueSize))
 					
 					#tell the main thread that all processing is done
 					#can only happen once all dirs have been exhausted
 					if queueSize == 0:
 						with ProcessingFinished:
+							print("notifying main thread")
 							ProcessingFinished.notify()
 
 
@@ -72,21 +117,27 @@ class ConsumerThread(threading.Thread):
 def queueEmpty():
 	return queueSize == 0
 	
-def main():
+def main(initialPathA, initialPathB, numThreads = 350):
+	global queueSize, hasBeenInit, diffSolu
 
-    initialPathA = 'Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetA'
-    initialPathB = 'Testing/DifferentDirectoryStructureDifferentFilesNested/DiffTargetB'
-    initialPath = (initialPathA, initialPathB)
-    q.put(initialPath)
-    numThreads = 1
-    for consumerNum in range(1, numThreads + 1):
-        c = ConsumerThread()
-        c.daemon = True
-        c.start()
+	#initialize threads the first time
+	if hasBeenInit != True:
+		hasBeenInit = True
+		for consumerNum in range(1, numThreads + 1):
+			c = ConsumerThread()
+			c.daemon = True
+			c.start()
+	
+	#reset globals
+	queueSize = 1
+	diffSolu = {"files":[], "dirs":[]}
+	
+	#add first path to the queue. Processing will fill up diffSolu
+	initialPath = (initialPathA, initialPathB)
+	q.put(initialPath)
     
 	#main thread waits for all processing to be done, then kills the program
-    with ProcessingFinished:
-    	ProcessingFinished.wait_for(queueEmpty)
-    	return 0
-
-main()
+	with ProcessingFinished:
+		ProcessingFinished.wait_for(queueEmpty)
+		pprint.pprint(diffSolu, width=300)
+		return diffSolu

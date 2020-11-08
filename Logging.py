501acb1b5494ed9519702e2346d6ac417f7050f0
modelarious <michaelwilliamhackman@gmail.com>
2020-11-07 20:35:04 -0700

    increase logging

--- Logging.py
+++ Logging.py
@@ -2,12 +2,12 @@
 DEBUG and STATUS are for debug purposes and just change the level of logging that is occuring.
 These don't ever need to be used unless you want more detail into what is going on
 '''
-DEBUG = False
+DEBUG = True
 def printDEBUG(*args):
 	if DEBUG == True:
 		print(*args)
 
-STATUS = False
+STATUS = True
 def printSTATUS(*args):
 	if STATUS == True:
 		print(*args)
